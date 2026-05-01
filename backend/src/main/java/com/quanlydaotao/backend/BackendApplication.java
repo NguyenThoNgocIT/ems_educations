b55dcd0ac081a8292682763799b567f181a0e97a
NguyenThoNgocIT <nguyenthongoc22072004@gmail.com>
2026-05-01 22:56:40 +0700

    cấu hình API document với Swagger

--- backend/src/main/java/com/quanlydaotao/backend/BackendApplication.java
+++ backend/src/main/java/com/quanlydaotao/backend/BackendApplication.java
@@ -2,14 +2,11 @@ package com.quanlydaotao.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class BackendApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(BackendApplication.class, args);
 
 	}
