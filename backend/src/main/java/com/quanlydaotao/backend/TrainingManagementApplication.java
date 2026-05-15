ea8f8f3090a32bc8a552f71f0979234dd34f5279
NguyenThoNgocIT <nguyenthongoc22072004@gmail.com>
2026-05-15 20:16:42 +0700

    update instruct chỉnh workflow

--- backend/src/main/java/com/quanlydaotao/backend/TrainingManagementApplication.java
+++ backend/src/main/java/com/quanlydaotao/backend/TrainingManagementApplication.java
@@ -1,11 +1,5 @@
 package com.quanlydaotao.backend;
-
-import com.quanlydaotao.backend.role.entity.Role;
 import com.quanlydaotao.backend.role.repository.RoleRepository;
-import com.quanlydaotao.backend.user.entity.Person;
-import com.quanlydaotao.backend.user.entity.User;
-import com.quanlydaotao.backend.user.entity.UserRole;
-import com.quanlydaotao.backend.user.entity.UserRoleId;
 import com.quanlydaotao.backend.user.repository.PersonRepository;
 import com.quanlydaotao.backend.user.repository.UserRepository;
 import com.quanlydaotao.backend.user.repository.UserRoleRepository;
--- backend/src/main/java/com/quanlydaotao/backend/account/service/impl/AccountServiceImpl.java
+++ backend/src/main/java/com/quanlydaotao/backend/account/service/impl/AccountServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.format.DateTimeFormatter;
-import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl {
